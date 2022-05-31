f74c642fe144acfe687f086abb770b4cf812eb5a
Nyasha1998 <blessedmanox@gmail.com>
2022-05-31 18:55:16 +0200

    updates

--- src/components/NegativeFeedback/FeedbackStyle.js
+++ src/components/NegativeFeedback/FeedbackStyle.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   height: 50vh;
-  width: 100vh;
+  background-color: #00888B;
   border-radius: 0.5rem;
   display: flex;
   flex-direction: column;
